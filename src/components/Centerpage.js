f1bc27490264a3e7d3068f195b7013cd3d57f8df
nadhoir <attoumani.nadhoir@gmail.com>
2022-01-28 21:57:36 +0100

    0.1 init

--- src/components/Centerpage.js
+++ src/components/Centerpage.js
@@ -6,6 +6,18 @@ function Centerpage() {
       <header>
         <h1>ACCUEIL</h1>
       </header>
+      <div>
+        <div className=" status">
+          <img
+            src="https://pbs.twimg.com/profile_images/1482759016535269379/m559Oniw_400x400.jpg"
+            className="img-rounded "
+          />
+          <input placeholder="Quoi de neuf ?" className="status_input" />
+          <div className="reponse">
+            <span>Tout le monde peut répondre</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
